49c0153ed55522564f290ea9e264cba79f3a29dd
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-06 00:19:44 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstChar(text) {
   // your code here
+	var char ="";
+  for(let i = 0; i<text.length; i++)
+  {
+      if(text[i] !=" ")
+      {
+          char += text[i];
+          break;
+      }
+  }
+  return char;
 }
 
 // Do not change the code below
